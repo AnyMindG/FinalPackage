bbf7bcdc95026b282db6341cc398b6b562ddb3cd
narender <narender@anymindgroup.com>
2025-01-31 19:42:34 +0530

    Updating Swift Package to 1.1.0

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FinalPackage",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(
+            name: "FinalPackage",
+            targets: ["FinalPackage"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AnyMindG/InMobiAdSDK.git", from: "1.1.0"),
+        .package(url: "https://github.com/AnyMindG/GoogleMobileAdsMediationInMobi.git", from: "1.1.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.13.0"),
+    ],
+    targets: [
+        .target(
+            name: "FinalPackage",
+            dependencies: [
+                .product(name: "InMobiAdSDK", package: "InMobiAdSDK"),
+                .product(name: "GoogleMobileAdsMediationInMobi", package: "GoogleMobileAdsMediationInMobi"),
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+            ]
+        ),
+        .testTarget(
+            name: "FinalPackageTests",
+            dependencies: ["FinalPackage"]
+        ),
+    ]
+)
