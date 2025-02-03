b9b3587c0f5ff047028216c5b4dd1053f8574bf5
narender <narender@anymindgroup.com>
2025-02-03 14:51:25 +0530

    Updating Swift Package to 1.3.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-// FinalPackage v1.2.0
+// FinalPackage v1.3.0
 let package = Package(
     name: "FinalPackage",
     platforms: [.iOS(.v12)],
@@ -25,7 +25,24 @@ let package = Package(
                 .product(name: "InMobiAdSDK", package: "InMobiAdSDK"),
                 .product(name: "GoogleMobileAdsMediationInMobi", package: "GoogleMobileAdsMediationInMobi"),
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
-            ]
+            ],
+            linkerSettings: [
+                            .linkedLibrary("sqlite3.0.tbd"),
+                            .linkedLibrary("libz.tbd"),
+                            .linkedFramework("WebKit"),
+                            .linkedFramework("AdSupport"),
+                            .linkedFramework("AVFoundation"),
+                            .linkedFramework("CoreGraphics"),
+                            .linkedFramework("CoreMedia"),
+                            .linkedFramework("CoreTelephony"),
+                            .linkedFramework("StoreKit"),
+                            .linkedFramework("SystemConfiguration"),
+                            .linkedFramework("UIKit"),
+                            .linkedFramework("AudioToolbox"),
+                            .linkedFramework("CFNetwork"),
+                            .linkedFramework("MediaPlayer"),
+                            .linkedFramework("QuartzCore"),
+                        ]
         ),
         .testTarget(
             name: "FinalPackageTests",
