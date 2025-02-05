369c1f4df325e1a05beac25dccb5957db87b6abe
narender <111370321+AnyMindG@users.noreply.github.com>
2025-02-05 21:00:12 +0530

    Updating Swift Package to 1.11.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-// v.1.10.0
+// v.1.11.0
 let package = Package(
     name: "FinalPackage",
     platforms: [.iOS(.v12)],
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/AnyMindG/PAGAdSDK.git", from: "1.4.0"),
+        .package(url: "https://github.com/AnyMindG/PAGAdSDK.git", from: "1.5.0"),
         .package(url: "https://github.com/AnyMindG/GoogleMobileAdsMediationPangle.git", from: "1.2.0"),
         .package(url: "https://github.com/AnyMindG/InMobiAdSDK.git", from: "1.1.0"),
         .package(url: "https://github.com/AnyMindG/GoogleMobileAdsMediationInMobi.git", from: "1.2.0"),
