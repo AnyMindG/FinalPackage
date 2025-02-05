004b2e4b42cad3c9b5092e734dc18bc13eb6018c
narender <111370321+AnyMindG@users.noreply.github.com>
2025-02-05 14:43:24 +0530

    Updating Swift Package to 1.7.0

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@
 
 import PackageDescription
 
+// v.1.7.0
 let package = Package(
     name: "FinalPackage",
     platforms: [.iOS(.v12)],
@@ -13,26 +14,18 @@ let package = Package(
         ),
     ],
     dependencies: [
+        .package(url: "https://github.com/AnyMindG/PAGAdSDK.git", from: "1.1.0"),
+        .package(url: "https://github.com/AnyMindG/GoogleMobileAdsMediationPangle.git", from: "1.1.0"),
         .package(url: "https://github.com/AnyMindG/InMobiAdSDK.git", from: "1.1.0"),
         .package(url: "https://github.com/AnyMindG/GoogleMobileAdsMediationInMobi.git", from: "1.2.0"),
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.13.0"),
     ],
     targets: [
-        .binaryTarget(
-            name: "PAGAdSDK",
-            url: "https://github.com/AnyMindG/AllFrameworks/releases/download/10000.3.0/PAGAdSDK.zip",
-            checksum: "c6363d4f71824814089dd40be94448484e73d930ecb0e18e7586d03f9dcbda35" // Generate checksum using `shasum -a 256 <file>`
-        ),
-        .binaryTarget(
-            name: "GoogleMobileAdsMediationPangle",
-            url: "https://github.com/AnyMindG/AllFrameworks/releases/download/10000.4.0/GoogleMobileAdsMediationPangle.zip",
-            checksum: "8663f4596e6f95362562593e0f499706a696c8159f573a363bcf276dc801495a" // Generate checksum using `shasum -a 256 <file>`
-        ),
         .target(
             name: "FinalPackage",
             dependencies: [
-                "PAGAdSDK",
-                "GoogleMobileAdsMediationPangle",
+                .product(name: "PAGAdSDK", package: "PAGAdSDK"),
+                .product(name: "GoogleMobileAdsMediationPangle", package: "GoogleMobileAdsMediationPangle"),
                 .product(name: "InMobiAdSDK", package: "InMobiAdSDK"),
                 .product(name: "GoogleMobileAdsMediationInMobi", package: "GoogleMobileAdsMediationInMobi"),
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
